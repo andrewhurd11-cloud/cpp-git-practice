e4a47405454ea00a4c0722597678f2b2195834c0
Andrew Hurd <andrew.hurd11@gmail.com>
2026-08-26 11:54:31 -0700

    Add feature branch message

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 int main() {
     std::cout<<  "Hello from Git!"  << std::endl;
 	std::cout << "This line was added by Student B." << std::endl;
-		std::cout << "Another change from Student A." <<std::ends;
+	std::cout << "Another change from Student A." <<std::ends;
+	std::cost << "Feature branch is working." <<std::ends;
     return 0;
 }
