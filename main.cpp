ac26af45feb1caef9a3a1275d70b7eefba42931f
Oren <oren.manalo@gmail.com>
2026-08-26 11:54:15 -0700

    Add feature branch message

--- main.cpp
+++ main.cpp
@@ -4,5 +4,6 @@ int main() {
     std::cout<<  "Hello from Git!"  << std::endl;
 	std::cout << "This line was added by Student B." << std::endl;
 		std::cout << "Another change from Student A." <<std::ends;
+	std::cout << "Feature branch is working." << std::endl;
     return 0;
 }
